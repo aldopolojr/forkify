86d509b8e8e30b4feebca1794cf64e887f3e9150
aldopolojr <aldopolojr@icloud.com>
2021-03-22 19:11:21 +0100

    fix api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'cd13d527-6bad-4cbe-aa8e-f2b6606489bc';
+export const KEY = 'b6de7f1d-d5c6-4126-bd98-7dbc8f70e289';
 export const MODAL_CLOSE_SEC = 2.5;
